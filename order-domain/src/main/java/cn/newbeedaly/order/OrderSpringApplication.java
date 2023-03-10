caa16dda0c943a71374522b4683196f21233d15f
newbeedaly <newbeedaly@163.com>
2023-03-10 10:44:29 +0800

    springboot init

--- order-domain/src/main/java/cn/newbeedaly/order/OrderSpringApplication.java
+++ order-domain/src/main/java/cn/newbeedaly/order/OrderSpringApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DubboOrderApplication {
+public class OrderSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboOrderApplication.class, args);
+        SpringApplication.run(OrderSpringApplication.class, args);
     }
-
 }
